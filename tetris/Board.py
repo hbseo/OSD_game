334148eaeaacd8c5767366b81c72965ee9982733
alchon <gusqja8753@gmail.com>
2017-06-06 21:07:31 +0900

    Update skeleton

---
+++ tetris/Board.py
@@ -0,0 +1,147 @@
+import pygame
+from Piece import *
+
+#               R    G    B
+WHITE       = (255, 255, 255)
+GRAY        = (185, 185, 185)
+BLACK       = (  0,   0,   0)
+RED         = (155,   0,   0)
+LIGHTRED    = (175,  20,  20)
+GREEN       = (  0, 155,   0)
+LIGHTGREEN  = ( 20, 175,  20)
+BLUE        = (  0,   0, 155)
+LIGHTBLUE   = ( 20,  20, 175)
+YELLOW      = (155, 155,   0)
+LIGHTYELLOW = (175, 175,  20)
+
+class Board:
+    COLLIDE_ERROR = {'no_error' : 0, 'right_wall':1, 'left_wall':2,
+                     'bottom':3, 'overlap':4}
+
+    def __init__(self, screen):
+        self.screen = screen
+        self.width = 10
+        self.height = 20
+        self.block_size = 25    # 5 * 5
+        self.board = []
+        for _ in range(self.height):
+            self.board.append([0]*self.width)
+        self.generate_piece()
+
+    def generate_piece(self):
+        self.piece = Piece()
+        self.piece_x, self.piece_y = 3,0
+
+    def absorb_piece(self):
+        for y, row in enumerate(self.piece):
+            for x, block in enumerate(row):
+                if block:
+                    self.board[y+self.piece_y][x+self.piece_x] = block
+        self.generate_piece()
+
+    def block_collide_with_board(self, x, y):
+        if x < 0:
+            return Board.COLLIDE_ERROR['left_wall']
+        elif x >= self.width:
+            return Board.COLLIDE_ERROR['right_wall']
+        elif y >= self.height:
+            return Board.COLLIDE_ERROR['bottom']
+        elif self.board[y][x]:
+            return Board.COLLIDE_ERROR['overlap']
+        return Board.COLLIDE_ERROR['no_error']
+
+    def collide_with_board(self, dx, dy):
+        """piece가 보드에 충돌하는지 확인"""
+        for y, row in enumerate(self.piece):
+            for x, block in enumerate(row):
+                if block:
+                    collide = self.block_collide_with_board(x=x+dx, y=y+dy)
+                    if collide:
+                        return collide
+        return Board.COLLIDE_ERROR['no_error']
+
+    def can_move_piece(self, dx, dy):
+        _dx = self.piece_x + dx
+        _dy = self.piece_y + dy
+        if self.collide_with_board(dx = _dx, dy = _dy):
+            return False
+        return True
+
+    def can_drop_piece(self):
+        return self.can_move_piece(dx=0, dy=1)
+
+    def try_rotate_piece(self, clockwise=True):
+        self.piece.rotate(clockwise)
+        collide = self.collide_with_board(dx=self.piece_x, dy=self.piece_y)
+        if not collide:
+            pass
+        elif collide == Board.COLLIDE_ERROR['left_wall']:
+            if self.can_move_piece(dx=1, dy=0):
+                self.move_piece(dx=1, dy=0)
+            elif self.can_move_piece(dx=2, dy=0):
+                self.move_piece(dx=2, dy=0)
+            else:
+                self.piece.rotate(not clockwise)
+        elif collide == Board.COLLIDE_ERROR['right_wall']:
+            if self.can_move_piece(dx=-1, dy=0):
+                self.move_piece(dx=-1, dy=0)
+            elif self.can_move_piece(dx=-2, dy=0):
+                self.move_piece(dx=-2, dy=0)
+            else:
+                self.piece.rotate(not clockwise)
+        else:
+            self.piece.rotate(not clockwise)
+
+    def move_piece(self, dx, dy):
+        if self.can_move_piece(dx, dy):
+            self.piece_x += dx
+            self.piece_y += dy
+
+    def drop_piece(self):
+        if self.can_drop_piece():
+            self.move_piece(dx=0, dy=1)
+        else:
+            self.absorb_piece()
+            self.delete_lines()
+
+    def full_drop_piece(self):
+        while self.can_drop_piece():
+            self.drop_piece()
+        self.drop_piece()
+
+    def rotate_piece(self, clockwise=True):
+        self.try_rotate_piece(clockwise)
+
+    def pos_to_pixel(self, x, y):
+        return self.block_size*x, self.block_size*(y-2)
+
+    def delete_line(self, y):
+        for y in reversed(range(1, y+1)):
+            self.board[y] = list(self.board[y-1])
+
+    def delete_lines(self):
+        remove = [y for y, row in enumerate(self.board) if all(row)]
+        for y in remove:
+            self.delete_line(y)
+
+    def game_over(self):
+        return sum(self.board[0]) > 0 or sum(self.board[1]) > 0
+
+    def draw_blocks(self, array2d, color=WHITE, dx=0, dy=0):
+        for y, row in enumerate(array2d):
+            y += dy
+            if y >= 2 and y < self.height:
+                for x, block in enumerate(row):
+                    if block:
+                        x += dx
+                        x_pix, y_pix = self.pos_to_pixel(x, y)
+                        # draw block
+                        pygame.draw.rect(self.screen, color,
+                                        (x_pix, y_pix, self.block_size, self.block_size))
+                        # draw border
+                        pygame.draw.rect(self.screen, (0,0,0),
+                                        (x_pix, y_pix, self.block_size, self.block_size), 1)
+
+    def draw(self):
+        self.draw_blocks(self.piece, dx=self.piece_x, dy=self.piece_y)
+        self.draw_blocks(self.board)
